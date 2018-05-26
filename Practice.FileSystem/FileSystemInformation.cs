729c98be3c6a7a9aae19a461cf5a3f019bddddd9
Ayaan <Ayaan@DESKTOP-31UNVUS>
2018-05-26 09:09:18 +0530

    Add project files.

---
+++ Practice.FileSystem/FileSystemInformation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Practice.FileSystem
+{
+	public class FileSystemInformation
+	{
+		public void CopyDeleteMove()
+		{
+
+			string filename = "sravani_text.txt";
+			string sourchepath = @"d:\FirstFile\MyTest";
+			string taretpath = @"D:\SecondFile\MyTest\TestFile";
+
+			string sourcefile = Path.Combine(sourchepath, filename);
+			string destfile = Path.Combine(taretpath, filename);
+
+			if (!File.Exists(sourchepath))
+			{
+				DirectoryInfo directory = new DirectoryInfo(sourchepath);
+				directory.Create();
+
+			}
+			else
+			{
+				Console.WriteLine("path exists");
+			}
+			if (!File.Exists(taretpath))
+			{
+				DirectoryInfo directory = new DirectoryInfo(taretpath);
+				directory.Create();
+			}
+			File.Copy(sourcefile, destfile,true);
+		}
+		public void GetFiles()
+		{
+			string sourcepath = @"d:\FirstFile\MyTest";
+			DirectoryInfo directory = new DirectoryInfo(sourcepath);
+			List<FileInfo> fileslist = directory.GetFiles("*.txt*").ToList(); ; 
+		}
+	}
+}
---
+++ Practice.FileSystem/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Practice.FileSystem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileSystemInformation fileSystem = new FileSystemInformation();
+			//fileSystem.CopyDeleteMove();
+			fileSystem.GetFiles();
+
+			//FileSysInfo filesysinfo = new FileSysInfo();
+			////filesysinfo.DriveInformation();
+			////filesysinfo.CreateFile();
+			//filesysinfo.CopyDeleteMove();
+
+
+		}
+	}
+	public class FileSysInfo
+	{
+		public void DriveInformation()
+		{
+			DriveInfo drive = new DriveInfo("c:");
+			var res=drive.AvailableFreeSpace;
+
+			DirectoryInfo directoryInfo = new DirectoryInfo(@"D:\NewMyFile1\Test");
+			directoryInfo.Create();
+			DirectorySecurity directorySecurity = new DirectorySecurity();
+			directoryInfo.Create();
+			//directoryInfo.CreateSubdirectory("MyTest");
+
+			//List<FileInfo> fileInfo = directoryInfo.GetFiles("*.*").ToList();
+
+			//foreach (var filelist in fileInfo)
+			//{
+			//	Console.WriteLine("{0},{1},{2}", filelist.Name, filelist.Length, filelist.LastAccessTime);
+			//	Console.ReadLine();
+
+			//}
+			 List<DirectoryInfo> directoryInfos=directoryInfo.GetDirectories(@"*.*").ToList();
+			foreach (var dirinfo in directoryInfos)
+			{
+				Console.WriteLine(dirinfo.Name);
+				Console.ReadLine();
+
+			}
+		}
+		public void CreateFile()
+		{
+			string drive = @"D:\NewMyFile1\";
+			string foldername = "Test";
+			string path = Path.Combine(drive, foldername);
+			//DirectoryInfo directoryInfo = new DirectoryInfo(path);
+			//directoryInfo.Create();
+			string filename = "Sravani_file.txt";
+			path = Path.Combine(path, filename);
+
+			if (File.Exists(path))
+			{
+				Console.WriteLine("file already exist");
+			}
+			else
+			{
+				FileStream fs = File.Create(path);
+				for (byte i = 0; i < 100; i++)
+				{
+					fs.WriteByte(i);
+				}
+
+			}
+			string text = "new text";
+		File.WriteAllText(path, text);
+
+			using (System.IO.StreamWriter file =
+		   new System.IO.StreamWriter(path, true))
+			{
+				file.WriteLine("\n Fourth line");
+			}
+		}
+
+		public void CopyDeleteMove()
+		{
+			string filename = "sravani.txt";
+			string sourcepath = @"D:\NewMyFile1\Test";
+			string targetpath = @"D:\NewMyFile2\Test2";
+			if (Directory.Exists(targetpath))
+			{
+
+				Directory.CreateDirectory(targetpath);
+			}
+			else
+			{
+				Console.WriteLine("directory exists");
+			}
+			File.Copy(sourcepath, targetpath);
+
+			if (Directory.Exists(sourcepath))
+			{
+				string[] files= Directory.GetFiles(sourcepath);
+				foreach (var str in files)
+				{
+					filename = Path.GetFileName(str);
+					var destfile = Path.Combine(targetpath, filename);
+					File.Copy(str, destfile, true);
+
+				}
+			}
+		}
+
+
+	}
+}
